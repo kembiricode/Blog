52f8b7d88186368da4403a97da25875648150006
kembiricode <161259760+kembiricode@users.noreply.github.com>
2024-03-06 11:50:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+  const hamMenu = document.querySelector('.ham-menu');
+var navSlide = document.querySelector('.container .nav-class');
+
+hamMenu.addEventListener('click',() => {
+  if(navSlide.style.transform === 'translateY(100%) translateX(0px)'){
+    return navSlide.style.transform = 'translateY(100%) translateX(100%)'
+  }
+  navSlide.style.transform = 'translateY(100%) translateX(0)'
+})
